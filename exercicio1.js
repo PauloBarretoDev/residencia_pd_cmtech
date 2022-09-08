04aa43d524b420aa986be62a373f4add93ab2340
Paulo Barreto Neto <105065779+PauloBarretoDev@users.noreply.github.com>
2022-09-08 19:52:35 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,3 @@
+var endereco = { rua: "Rua Professor Cláudio Selva", numero: 4, bairro: "Dois Irmãos", cidade: "Recife", uf: "PE" };
+
+console.log("O usuário mora em " + endereco.cidade + "/" + endereco.uf + ", no bairro " + endereco.bairro + ", na rua '" + endereco.rua + "' com nº" + endereco.numero);
